2ebd111b15d36aa4b4406deface5dc24144542f7
pavan-t <pavanthulluru@gmail.com>
2017-03-16 13:53:41 -0400

    Add files via upload

---
+++ scraping/scraping/scraping.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="scraping.aspx.cs" Inherits="scraping.scraping" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Label ID="Label1" runat="server" Text="Output_Label"></asp:Label>
+        <asp:ListBox ID="ListBox1" runat="server"></asp:ListBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="scrape" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ scraping/scraping/scraping.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HtmlAgilityPack;
+
+namespace scraping
+{
+    public partial class scraping : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+              var getHtmlWeb = new HtmlWeb(); 
+            var document = getHtmlWeb.Load(TextBox1.Text);            
+            var aTags = document.DocumentNode.SelectNodes("//a");            
+            int counter = 1;             
+            if (aTags != null)           
+            {               
+                foreach (var aTag in aTags)               
+                {                    
+                    Label1.Text += counter + ". " + aTag.InnerHtml + " - " + aTag.Attributes["href"].Value + "\t" + "<br/>";                   
+                    counter++;
+                }
+            }
+        }
+    }
+}
